c25e273f9515ff009a5806e609c50251369d05d0
Patrick Jones <patrickjones.pmj@gmail.com>
2020-03-04 22:39:54 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,7 +4,12 @@ require 'directors_database'
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+  gross_earnings = 0
+  index = 0
 
+pp director_data
+  # while index < director_data
+  # end
 end
 
 # Write a method that, given an NDS creates a new Hash
